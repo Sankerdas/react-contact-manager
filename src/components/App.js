98ba0ecb7a01cd6bd20237b8f14d29262300a3e9
Sanker das <sankerdas.yes@gmail.com>
2022-04-12 17:51:15 +0530

    contacts added dynamically
    react hooks used, contacts stored in localStorage
    
    child to parent communication though props
    this.props.addContactHandler(this.state);
    
    useState is a react hook used to manipulate the state
    
    useEffect is a react hook used to render the ui based on changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,34 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react'; 
+// useState is a react hook used to manipulate the state
+// useEffect is a react hook used to render the ui based on changes
+
 import AddContact from './AddContact';
 import './App.css';
 import ContactList from './ContactList';
 import Header from './Header';
 
 function App() {
-  const contacts = [
-    {id: '1', name: "sankerdas", email: 's@gmai.com' },
-    {id: '2', name: "john", email: 'j@gmai.com' },
-    {id: '3', name: "rahul", email: 'r@gmai.com' },
-  ]
+  const [contacts, setContacts] = useState([]); // define contacts as state & use setContacts to manupulate the state
+  const addContactHandler = (contact) => {
+    console.log(contact)
+    setContacts([...contacts, contact]);
+  }
+
+  useEffect(()=> {
+    const localContacts = JSON.parse( localStorage.getItem("contacts", JSON.stringify(contacts) ));
+    setContacts(localContacts)
+
+  }, []);
+
+  useEffect(()=> {
+    localStorage.setItem("contacts", JSON.stringify(contacts) )
+  }, [contacts])
+
   return (
     <div className="ui container">
         <Header/>
-        <AddContact/>
+        <AddContact addContactHandler={addContactHandler} />
         <ContactList contacts={contacts} />
     </div>
   );
